8bddd90b92a3c4de4ac0fdc691bdfb7c2a7fa711
Wheaxy Sueido <joshgunvk@code.edu.az>
2024-05-20 20:48:04 +0400

    Task 2 Ede Bilmedim :(

---
+++ 20052024/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        string password = "SeSioS29#2";
+        bool Valid = ValidatePassword(password);
+        Console.WriteLine($"Password succesfully - {Valid}");
+    }
+
+    public static bool ValidatePassword(string password)
+    {
+        if (password.Length < 8)
+        {
+            return false;
+        }
+
+        if (!char.IsUpper(password[0]))
+        {
+            return false;
+        }
+
+        if (!password.Any(char.IsDigit))
+        {
+            return false;
+        }
+
+        if (!password.Any(ch => !char.IsLetterOrDigit(ch)))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
